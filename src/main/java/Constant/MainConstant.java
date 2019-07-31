9427a9b1e0f60ee9fcd181546b8d4963a0a68330
BradYuShengDeng <deng.0124@gmail.com>
2019-08-01 01:50:28 +0800

    Constants - Complemented global constants.

--- src/main/java/Constant/MainConstant.java
+++ src/main/java/Constant/MainConstant.java
@@ -3,5 +3,8 @@ package Constant;
 public class MainConstant {
 
     public static final String BLANK = " ";
+    public static final String SHORT_SPACE = "     ";
+    public static final String LONG_SPACE = "          ";
+    public static final String NEW_LINE = "\r\n";
 
 }
