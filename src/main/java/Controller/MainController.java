5f3f9cccbe94c36f2c3067d3e514c942a93ea44a
BradYuShengDeng <deng.0124@gmail.com>
2019-08-01 00:20:46 +0800

    Controller - Completed controller methods by business needs.

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -1,39 +1,44 @@
 package Controller;
 
 import Constant.MainConstant;
+import Module.ParkingLot;
 import Service.MainService;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainController {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
         if(args.length == 0){ // Interactive command prompt's part
 
             String userInput;
-            Scanner sn = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
 
             while(true){
-                userInput = sn.next();
-                MainController.takeActionByGoal(userInput);
+                userInput = scanner.next();
+                String result = MainController.takeActionByGoal(userInput);
+                System.out.println(result);
             }
 
         }else if(args.length == 1){ // File as a parameter's part
 
             BufferedReader br = null;
+            BufferedWriter bw = null;
 
             try{
                 File f = new File(args[0]);
                 br = new BufferedReader(new FileReader(f));
+                File outputFile = new File("file_outputs.txt");
+                bw = new BufferedWriter(new FileWriter(outputFile));
                 String currentLine;
 
                 while((currentLine = br.readLine()) != null){
-                    MainController.takeActionByGoal(currentLine);
+                    String result = MainController.takeActionByGoal(currentLine);
+                    bw.write(result);
+                    bw.write(System.getProperty("line.separator"));
                 }
 
             }catch(IOException e){
@@ -42,6 +47,7 @@ public class MainController {
             }finally{
                 try{
                     if(br != null) br.close();
+                    if(bw != null) bw.close();
                 }catch(IOException e){
                     e.printStackTrace();
                 }
@@ -52,38 +58,40 @@ public class MainController {
         }
     }
 
-    private static void takeActionByGoal(String command){
+    private static String takeActionByGoal(String command) throws Exception{
 
         String[] segments = command.split(MainConstant.BLANK);
         String goal = segments[0];
         switch(goal){
             case "create_parking_lot":
-                MainService.createParkingLot(segments[1]);
-                break;
+                ParkingLot parkingLot = MainService.createParkingLot(segments[1]);
+                return "Created a parking lot with " + parkingLot.getSlots().size() + " slots";
 
             case "parking":
-                MainService.parkCar(segments[1], segments[2]);
-                break;
+                Integer number = MainService.parkCar(segments[1], segments[2]);
+                return number != null ? "Allocated slot number: " + number.intValue() : "Sorry, parking lot is full";
 
             case "leave":
-                MainService .leaveCar(segments[1]);
-                break;
+                MainService.leaveCar(segments[1]);
+                return "Slot number " + segments[1] + " is free";
 
             case "status":
                 MainService.checkStatus();
                 break;
 
             case "registration_numbers_for_cars_with_colour":
-                MainService.getRegistrationNumbersByColor(segments[1]);
-                break;
+                String[] registrationNumbers = MainService.getRegistrationNumbersByColor(segments[1]);
+                String regNums = Arrays.toString(registrationNumbers);
+                return regNums.substring(1, regNums.length() -1);
 
             case "slot_numbers_for_cars_with_colour":
-                MainService.getSlotNumbersByColor(segments[1]);
-                break;
+                Integer[] slotNumbers = MainService.getSlotNumbersByColor(segments[1]);
+                String slotNums = Arrays.toString(slotNumbers);
+                return slotNums.substring(1, slotNums.length() -1);
 
             case "slot_number_for_registration_number":
-                MainService.getSlotNumberByRegistrationNumber(segments[1]);
-                break;
+                Integer slotNumber = MainService.getSlotNumberByRegistrationNumber(segments[1]);
+                return slotNumber != null ? String.valueOf(slotNumber) : "Not found";
 
             default:
                 System.out.println("Invalid command:" + segments[0]);
