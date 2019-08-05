4711c4162a8de9028d27af8b54c856a296ddefff
BradYuShengDeng <deng.0124@gmail.com>
2019-08-05 17:37:18 +0800

    Messages - Added global messages.

---
+++ src/main/java/Message/MainMessage.java
@@ -0,0 +1,16 @@
+package Message;
+
+/**
+ * Author: Brad Yu-Sheng Deng
+ * Version: 1.0.0
+ * Description: This contains all global messages.
+ */
+public class MainMessage {
+
+    public static final String MSG_INVALID_COMMAND = "Error Msg: The following command is invalid: ";
+    public static final String MSG_ILLEGAL_ARGUMENT = "Error Msg: The number of arguments is incorrect";
+    public static final String MSG_NO_PARKING_LOT = "Error Msg: There is no parking lot created";
+    public static final String MSG_NO_SUCH_SLOT_NUMBER = "Error Msg: The slot number is invalid";
+    public static final String MSG_NO_SUCH_COLOR = "Error Msg: The color is invalid";
+
+}
